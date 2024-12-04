aef357a656e1b9ba9a1a29155951b1f29c052e4a
wellxsz <wzkillergamerzinho@gmail.com>
2024-12-04 16:10:41 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,33 @@
-document.getElementById('downloadBtn').addEventListener('click', function() {
-    window.location.href = 'https://github.com/wellxsz/JS_Undertale'; // Substitua pelo link real
+(function() {
+    emailjs.init('5tAhgDpIvV6eZ1lFG');  // Substitua com seu User ID
+})();
+
+document.getElementById("contact-form").addEventListener("submit", function(event) {
+    event.preventDefault();
+
+    // Exibe a mensagem de status (enviando)
+    document.getElementById("status-message").innerHTML = "Enviando...";
+
+    // Obter os valores do formulário
+    var name = document.getElementById('name').value;
+    var email = document.getElementById('email').value;
+    var message = document.getElementById('message').value;
+
+    // Enviar o e-mail usando o EmailJS
+    emailjs.send("service_6ldbzfd", "template_48t8jc8", {
+        from_name: name,
+        from_email: email,
+        message: message
+    })
+    .then(function(response) {
+        console.log("Mensagem enviada:", response);
+        document.getElementById("status-message").innerHTML = "Mensagem enviada com sucesso!";
+    }, function(error) {
+        console.log("Erro ao enviar a mensagem:", error);
+        document.getElementById("status-message").innerHTML = "Falha ao enviar a mensagem. Tente novamente.";
+    });
 });
+
+window.onload = function() {
+    document.querySelector('h1 span').classList.add('typing');
+};
---
+++ script_undertale.js
@@ -0,0 +1,3 @@
+document.getElementById('downloadBtn').addEventListener('click', function() {
+    window.location.href = 'https://github.com/wellxsz/JS_Undertale'; // Substitua pelo link real
+});
